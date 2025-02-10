0a7b7187a48e406d00982a45964a31bf1e446584
boscocj16 <boscojoseph79@gmail.com>
2025-02-10 23:02:35 +0530

    2.19

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,12 @@ import { useState, useEffect } from "react";
 function CountrySearchApp() {
   const [query, setQuery] = useState("");
   const [countries, setCountries] = useState([]);
+  const [selectedCountry, setSelectedCountry] = useState(null);
   
   useEffect(() => {
     if (query.length < 1) {
       setCountries([]);
+      setSelectedCountry(null);
       return;
     }
 
@@ -29,29 +31,48 @@ function CountrySearchApp() {
         onChange={(e) => setQuery(e.target.value)}
       />
 
-      {countries.length > 10 && <p>Too many matches, be more specific.</p>}
+      <div>
+        {countries.length > 10 && <p>Too many matches, be more specific.</p>}
 
-      {countries.length > 1 && countries.length <= 10 && (
-        <ul>
-          {countries.map((country) => (
-            <li key={country.cca3}>{country.name.common}</li>
-          ))}
-        </ul>
-      )}
+        {countries.length > 1 && countries.length <= 10 && (
+          <ul>
+            {countries.map((country) => (
+              <li key={country.cca3}>
+                {country.name.common}
+                <button onClick={() => setSelectedCountry(country)}>show</button>
+              </li>
+            ))}
+          </ul>
+        )}
 
-      {countries.length === 1 && (
-        <div>
-          <h2>{countries[0].name.common}</h2>
-          <p>capital {countries[0].capital?.[0] || "N/A"}</p>
-          <p>area {countries[0].area}</p>
-          <p>languages {Object.values(countries[0].languages || {}).join(", ")}</p>
-          <img
-            src={countries[0].flags.png}
-            alt={`Flag of ${countries[0].name.common}`}
-            width="100"
-          />
-        </div>
-      )}
+        {selectedCountry && (
+          <div>
+            <h2>{selectedCountry.name.common}</h2>
+            <p>capital {selectedCountry.capital?.[0] || "N/A"}</p>
+            <p>area {selectedCountry.area}</p>
+            <p>languages {Object.values(selectedCountry.languages || {}).join(", ")}</p>
+            <img
+              src={selectedCountry.flags.png}
+              alt={`Flag of ${selectedCountry.name.common}`}
+              width="100"
+            />
+          </div>
+        )}
+
+        {countries.length === 1 && !selectedCountry && (
+          <div>
+            <h2>{countries[0].name.common}</h2>
+            <p>capital {countries[0].capital?.[0] || "N/A"}</p>
+            <p>area {countries[0].area}</p>
+            <p>languages {Object.values(countries[0].languages || {}).join(", ")}</p>
+            <img
+              src={countries[0].flags.png}
+              alt={`Flag of ${countries[0].name.common}`}
+              width="100"
+            />
+          </div>
+        )}
+      </div>
     </div>
   );
 }
